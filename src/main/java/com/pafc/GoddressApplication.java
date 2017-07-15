a606e1e1b280aa96d08089ef2230abccd9a4ca5f
TianYaKe <liulong@hzins.com>
2017-07-15 21:50:02 +0800

    用idea创建spring-boot项目

---
+++ src/main/java/com/pafc/GoddressApplication.java
@@ -0,0 +1,12 @@
+package com.pafc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoddressApplication.class, args);
+	}
+}
