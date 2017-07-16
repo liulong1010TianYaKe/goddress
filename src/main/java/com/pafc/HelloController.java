ad9acde868eab57d79a7dbb0e8482a5e4d17fb58
TianYaKe <liulong@hzins.com>
2017-07-16 09:59:26 +0800

    使用yml 文件

--- src/main/java/com/pafc/HelloController.java
+++ src/main/java/com/pafc/HelloController.java
@@ -1,7 +1,18 @@
 package com.pafc;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by coco on 17/7/16.
  */
+
+@RestController
 public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public  String  say(){
+        return "Hello Spring Boot!";
+    }
 }
