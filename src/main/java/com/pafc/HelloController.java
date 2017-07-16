534f3406d4b6698593340c68a62bd3a4abd63bdf
TianYaKe <liulong@hzins.com>
2017-07-16 10:18:52 +0800

    iml

--- src/main/java/com/pafc/HelloController.java
+++ src/main/java/com/pafc/HelloController.java
@@ -1,5 +1,7 @@
 package com.pafc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    public HelloController() {
+        super();
+    }
+
+    @Autowired
+    private GirlProperties girlProperties;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public  String  say(){
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot!"+girlProperties.getCupSize();
     }
 }
